53858e5f33ba2c843a7d24de49215277041fddc0
sharma jayesh <2304030101625@silveroakuni.ac.in>
2025-12-15 13:20:47 +0530

    js

---
+++ LeapYear(year) .js
@@ -0,0 +1,8 @@
+function isLeapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return "Leap Year";
+  } else {
+    return "Not a Leap Year";
+  }
+}
+
---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+
+let n = 10;
+
+
+let a = 0;
+let b = 1;
+
+console.log(a); 
+
+for (let i = 1; i < n; i++) {
+    console.log(b); 
+    let next = a + b; 
+    a = b;            
+    b = next;        
+}
